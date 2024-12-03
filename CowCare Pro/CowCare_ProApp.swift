55812aabdf443ef42aca8f4715da83bb414c7f4a
dsv <zxcv>
2024-12-04 01:22:42 +0500

    Initial Commit

---
+++ CowCare Pro/CowCare_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowCare_ProApp.swift
+//  CowCare Pro
+//
+//  Created by Muhammad Rizwan on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CowCare_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
